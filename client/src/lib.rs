661f2c45d8ba5073fcad7cf59ae82124601e2289
Masterchef365 <duncan.freeman1@gmail.com>
2024-06-17 23:31:51 -0700

    htiaslj;kdfnahi

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,7 +1,8 @@
 use std::{net::ToSocketAddrs, sync::Arc};
 
-use egui::{mutex::Mutex, Id, Ui, Vec2, Widget};
+use egui::{mutex::Mutex, Id, RichText, Ui, Vec2, Widget};
 
+#[derive(Clone)]
 pub struct ServerView {
     addr: String,
     size: Vec2,
@@ -21,7 +22,7 @@ impl Widget for ServerView {
         let client = ui.ctx().memory_mut(|mem| {
             mem.data
                 .get_temp_mut_or_insert_with(Id::new(&self.addr), || {
-                    Arc::new(Mutex::new(Client::new(self.addr.clone())))
+                    Arc::new(Mutex::new(Client::new(self.clone())))
                 })
                 .clone()
         });
@@ -37,22 +38,34 @@ pub enum Client {
 }
 
 impl Client {
-    fn new(addr: String) -> Self {
-        match ewebsock::connect(addr, Default::default()) {
-            Ok((tx, rx)) => Self::Success(ClientImpl { rx, tx }),
+    fn new(view: ServerView) -> Self {
+        match ewebsock::connect(&view.addr, Default::default()) {
+            Ok((tx, rx)) => Self::Success(ClientImpl { rx, tx, view }),
             Err(e) => Self::Failure {
-                addr,
-                error: format!("{#:?}", e),
+                addr: view.addr,
+                error: format!("{:?}", e),
             },
         }
     }
 
     fn show(&mut self, ui: &mut Ui) -> egui::Response {
-        todo!()
+        match self {
+            Self::Failure { addr, error } => {
+                ui.label(format!("Error connecting to {addr}; {error}"))
+            }
+            Self::Success(client) => client.show(ui),
+        }
     }
 }
 
 struct ClientImpl {
     tx: ewebsock::WsSender,
     rx: ewebsock::WsReceiver,
+    view: ServerView,
+}
+
+impl ClientImpl {
+    fn show(&mut self, ui: &mut Ui) -> egui::Response {
+        ui.label("TODO")
+    }
 }
