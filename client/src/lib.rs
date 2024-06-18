2f1f87d8b20013c39041f9aed2031136306339de
Masterchef365 <duncan.freeman1@gmail.com>
2024-06-17 23:20:23 -0700

    Thinking

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,7 +1,45 @@
-use std::net::ToSocketAddrs;
+use std::{net::ToSocketAddrs, sync::Arc};
 
-use egui::Ui;
+use egui::{mutex::Mutex, Id, Ui, Vec2, Widget};
 
-fn view_server_ui(ui: &Ui, addr: impl ToSocketAddrs, size: egui::Vec2) {
-    //   I/O   w/ server
+pub struct ServerView {
+    addr: String,
+    size: Vec2,
+}
+
+impl ServerView {
+    pub fn new(addr: String) -> Self {
+        Self {
+            addr,
+            size: Vec2::new(200., 200.),
+        }
+    }
+}
+
+impl Widget for ServerView {
+    fn ui(self, ui: &mut Ui) -> egui::Response {
+        let client = ui.ctx().memory_mut(|mem| {
+            mem.data
+                .get_temp_mut_or_insert_with(Id::new(&self.addr), || {
+                    Arc::new(Mutex::new(Client::new(self.addr.clone())))
+                })
+                .clone()
+        });
+
+        let mut lck = client.lock();
+        lck.show(ui)
+    }
+}
+
+pub struct Client {
+
+}
+
+impl Client {
+    fn new(addr: String) -> Self {
+    }
+
+    fn show(&mut self, ui: &mut Ui) -> egui::Response {
+        todo!()
+    }
 }
