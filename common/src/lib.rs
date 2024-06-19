fe7fab122f2d8fce037b9c040d418b3b78484f77
Masterchef365 <duncan.freeman1@gmail.com>
2024-06-19 16:44:10 -0700

    JSON protocol option

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -12,10 +12,23 @@ pub struct ClientToServer {
     pub raw_input: egui::RawInput,
 }
 
+#[cfg(feature = "bincode")]
 pub fn serialize<T: Serialize>(val: &T) -> bincode::Result<Vec<u8>> {
     bincode::serialize(val)
 }
 
+#[cfg(feature = "bincode")]
 pub fn deserialize<T: DeserializeOwned>(bytes: &[u8]) -> bincode::Result<T> {
     bincode::deserialize(bytes)
 }
+
+#[cfg(feature = "json")]
+pub fn serialize<T: Serialize>(val: &T) -> serde_json::Result<Vec<u8>> {
+    serde_json::to_vec(val)
+}
+
+#[cfg(feature = "json")]
+pub fn deserialize<T: DeserializeOwned>(bytes: &[u8]) -> serde_json::Result<T> {
+    serde_json::from_slice(bytes)
+}
+
