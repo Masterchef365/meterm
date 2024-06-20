b57b1f8ec263d37d18dfd7f0e4f4383b1047745c
Masterchef365 <duncan.freeman1@gmail.com>
2024-06-20 02:28:56 -0700

    Paint almost works

---
+++ server/examples/paint.rs
@@ -0,0 +1,99 @@
+use std::time::Instant;
+
+use metacontrols_common::egui::{Color32, Pos2, Rect, Sense, Stroke, Ui};
+use metacontrols_server::{egui::{self, DragValue, Slider}, Server};
+
+fn main() {
+    env_logger::try_init().unwrap();
+
+    let mut server = Server::new("0.0.0.0:5000");
+
+    let mut painting = Painting::default();
+
+    let desired_tickrate = 180.0;
+
+    loop {
+        let tick_start = Instant::now();
+
+        server.show_on_clients(|ctx, _user| {
+            egui::CentralPanel::default().show(ctx, |ui| {
+                painting.ui_control(ui);
+                painting.ui_content(ui);
+            });
+        });
+
+        let tick_time = tick_start.elapsed();
+        let remaining_time = (1. / desired_tickrate - tick_time.as_secs_f32()).max(0.0);
+        std::thread::sleep(std::time::Duration::from_secs_f32(remaining_time));
+    }
+}
+
+pub struct Painting {
+    /// in 0-1 normalized coordinates
+    lines: Vec<Vec<Pos2>>,
+    stroke: Stroke,
+}
+
+impl Default for Painting {
+    fn default() -> Self {
+        Self {
+            lines: Default::default(),
+            stroke: Stroke::new(1.0, Color32::from_rgb(25, 200, 100)),
+        }
+    }
+}
+
+impl Painting {
+    pub fn ui_control(&mut self, ui: &mut egui::Ui) -> egui::Response {
+        ui.horizontal(|ui| {
+            ui.label("Stroke:");
+            //ui.add(&mut self.stroke);
+            ui.separator();
+            if ui.button("Clear Painting").clicked() {
+                self.lines.clear();
+            }
+        })
+        .response
+    }
+
+    pub fn ui_content(&mut self, ui: &mut Ui) -> egui::Response {
+        let (mut response, painter) =
+            ui.allocate_painter(ui.available_size_before_wrap(), Sense::drag());
+
+        let to_screen = egui::emath::RectTransform::from_to(
+            Rect::from_min_size(Pos2::ZERO, response.rect.square_proportions()),
+            response.rect,
+        );
+        let from_screen = to_screen.inverse();
+
+        if self.lines.is_empty() {
+            self.lines.push(vec![]);
+        }
+
+        let current_line = self.lines.last_mut().unwrap();
+
+        if let Some(pointer_pos) = response.interact_pointer_pos() {
+            let canvas_pos = from_screen * pointer_pos;
+            if current_line.last() != Some(&canvas_pos) {
+                current_line.push(canvas_pos);
+                response.mark_changed();
+            }
+        } else if !current_line.is_empty() {
+            self.lines.push(vec![]);
+            response.mark_changed();
+        }
+
+        let shapes = self
+            .lines
+            .iter()
+            .filter(|line| line.len() >= 2)
+            .map(|line| {
+                let points: Vec<Pos2> = line.iter().map(|p| to_screen * *p).collect();
+                egui::Shape::line(points, self.stroke)
+            });
+
+        painter.extend(shapes);
+
+        response
+    }
+}
