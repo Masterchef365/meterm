8d0847c2aa9b8367d06b520350abf1e7542274b7
Masterchef365 <duncan.freeman1@gmail.com>
2024-06-19 20:15:20 -0700

    Demo lib

---
+++ examples/basic/using_demo_lib/src/main.rs
@@ -0,0 +1,30 @@
+use std::time::Instant;
+
+use egui_demo_lib::DemoWindows;
+use metacontrols_server::{egui, Server};
+
+fn main() {
+    env_logger::try_init().unwrap();
+
+    let mut server = Server::new("0.0.0.0:5000");
+
+    let mut counter = 0;
+
+    // We want 20 ticks per second
+    let desired_tickrate = 90.0;
+
+    loop {
+        let tick_start = Instant::now();
+
+        let mut demo = DemoWindows::default();
+
+        server.show_on_clients(|ctx| {
+            demo.ui(ctx);
+        });
+
+        let tick_time = tick_start.elapsed();
+        let remaining_time = (1. / desired_tickrate - tick_time.as_secs_f32()).max(0.0);
+        std::thread::sleep(std::time::Duration::from_secs_f32(remaining_time));
+    }
+}
+
